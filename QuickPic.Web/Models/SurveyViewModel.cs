00fdd115d388c9935935570f6e8a2fbfe192b3e3
Xander Coetzee <coetzeex@outlook.com>
2021-04-21 07:13:06 -0800

    Created a view model for the survey.

---
+++ QuickPic.Web/Models/SurveyViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickPic.Web.Models
+{
+    public class SurveyViewModel
+    {
+        public int QuestionId { get; set; }
+
+        public string QuestionText { get; set; }
+
+        public int Answer { get; set; }
+    }
+}
