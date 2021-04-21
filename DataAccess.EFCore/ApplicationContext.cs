5fae36ad974dd49af1edb3a2a31a452639b1abb8
Xander Coetzee <coetzeex@outlook.com>
2021-04-20 22:01:46 -0800

    Created an application context for EF.

---
+++ DataAccess.EFCore/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.EFCore
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Respondent> Respondents { get; set; }
+    }
+}
