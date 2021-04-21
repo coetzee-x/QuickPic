8071ee5731e23644a6c28eb7ebf16f48a9364fe7
Xander Coetzee <coetzeex@outlook.com>
2021-04-20 22:07:09 -0800

    Added the application context to the startup with a connection string.

--- QuickPic.Web/Startup.cs
+++ QuickPic.Web/Startup.cs
@@ -1,6 +1,8 @@
+using DataAccess.EFCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,10 @@ namespace QuickPic.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(ApplicationContext).Assembly.FullName)));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
