848e07c0393df7bab5c2e247625c940b2beaac0e
Xander Coetzee <coetzeex@outlook.com>
2021-04-21 07:18:21 -0800

    Create index method for survey controller and return model to view.

--- QuickPic.Web/Controllers/SurveyController.cs
+++ QuickPic.Web/Controllers/SurveyController.cs
@@ -1,9 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using QuickPic.Web.Models;
+using System.Collections.Generic;
 
 namespace QuickPic.Web.Controllers
 {
     public class SurveyController : Controller
     {
+        private readonly IQuestionRepository _questionRepository;
 
+        public SurveyController(IQuestionRepository questionRepository)
+        {
+            _questionRepository = questionRepository;
+        }
+        public ActionResult Index()
+        {
+            var questions = _questionRepository.GetAll();
+
+            List<SurveyViewModel> model = new List<SurveyViewModel>();
+
+            foreach (var question in questions)
+            {
+                model.Add(new SurveyViewModel
+                {
+                    QuestionId = question.Id,
+                    QuestionText = question.Text,
+                    Answer = 0
+                });
+            }
+
+            return View(model);
+        }
     }
 }
